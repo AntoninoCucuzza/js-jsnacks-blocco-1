c644f324ea91da916944965b0b0c910a16bc6047
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-08-29 15:05:24 +0200

    end snack1 start snack2

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,8 +1,21 @@
 console.log('we');
-
 /*
 
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 
 */
+
+const userFirst = prompt('inserisci un numero')
+console.log(Number(userFirst));
+
+const userSecond = prompt('inserisci un numero')
+console.log(Number(userSecond));
+
+if(userFirst > userSecond){
+    console.log('il primo numero inserito e maggiore');
+}else if(userSecond > userFirst){
+    console.log('il secondo numero inserito e maggiore ');
+}else{
+    console.log('inserisci solo numeri');   
+}
